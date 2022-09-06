4cac8c1e59ccb2518829283e0d4548954260b849
torone89 <torone89>
2022-09-06 14:32:23 +0200

    Qs2

--- queries.sql
+++ queries.sql
@@ -13,9 +13,10 @@ FROM `students`
 WHERE YEAR (`date_of_birth`) = '1990';
 
 
-
-
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
+
+SELECT * FROM `courses`
+WHERE `cfu` > 10
 
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
