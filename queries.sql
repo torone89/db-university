e1aa1b2d97bb8c809cc4fee28e47e9cd96dcf32f
torone89 <torone89>
2022-09-06 14:55:02 +0200

    qs5

--- queries.sql
+++ queries.sql
@@ -38,7 +38,10 @@ WHERE `year` = 1 AND `period` = 'I semestre'
 
 
 
--- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+-- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del   (21)
+
+SELECT * FROM `exams`
+WHERE `date` = '20-06-20' AND `hour`> '14:00:00'
 
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
