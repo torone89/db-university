0e2fa487eced49450d0509d41d173f292b042040
torone89 <torone89>
2022-09-06 14:49:39 +0200

    4QS

--- queries.sql
+++ queries.sql
@@ -33,6 +33,10 @@ WHERE `date_of_birth` < '1992-01-01';
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
+SELECT * FROM `courses`
+WHERE `year` = 1 AND `period` = 'I semestre'  
+
+
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
