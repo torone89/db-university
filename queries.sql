cd4d23e74ce24e909c589b305acdeafcd3279703
torone89 <torone89>
2022-09-06 14:57:48 +0200

    qs6

--- queries.sql
+++ queries.sql
@@ -45,6 +45,9 @@ WHERE `date` = '20-06-20' AND `hour`> '14:00:00'
 
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
+SELECT * 
+FROM `degrees`
+WHERE `level` = 'magistrale'
 
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
