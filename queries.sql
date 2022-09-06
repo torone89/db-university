7bd08acf7b0a164f0b528c81a2da96588a89c80e
torone89 <torone89>
2022-09-06 15:03:08 +0200

    Queries Select Completed  (8)

--- queries.sql
+++ queries.sql
@@ -56,6 +56,10 @@ FROM `departments`
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
+  SELECT *
+    FROM `teachers`
+    WHERE `phone` IS NOT NULL
+
 
 
 -- ------------------------------------------ QUERY CON GROUP BY
