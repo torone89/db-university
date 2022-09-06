5d3066ca152828aa550e4ae97e704fe3caae88f4
torone89 <torone89>
2022-09-06 14:40:46 +0200

    q3

--- queries.sql
+++ queries.sql
@@ -21,6 +21,15 @@ WHERE `cfu` > 10
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
 
+SELECT * 
+FROM `students`
+WHERE YEAR(date_of_birth) <= 1991
+
+ALTERNATIVA
+
+SELECT * 
+FROM `students`
+WHERE `date_of_birth` < '1992-01-01';
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
