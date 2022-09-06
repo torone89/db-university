e8b57edf31cf91ea94f259e6c7db2d0849feaa9e
torone89 <torone89>
2022-09-06 14:59:43 +0200

    qs7

--- queries.sql
+++ queries.sql
@@ -51,7 +51,8 @@ WHERE `level` = 'magistrale'
 
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
-
+SELECT COUNT(*) as 'Numeri Dipartimenti' 
+FROM `departments`
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
