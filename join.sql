91d8bc56933b07e1d266e6bb4d10d1df6da215fa
torone89 <torone89>
2022-09-07 15:48:40 +0200

    5. ok

--- join.sql
+++ join.sql
@@ -4,7 +4,7 @@ SELECT `students`.*
     FROM `degrees`
     JOIN `students`
     ON `degrees`.`id` = `students`.`degree_id`
-  WHERE `degrees`.`name` = 'Corso di Laurea in Economia'
+  WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 *USO SHORTCUT per differenziare dal 1.
@@ -13,7 +13,7 @@ SELECT `A`.`name` AS 'Dipartimento', `B`.`name` AS 'Corso di Laurea'
 FROM `departments` A
 INNER JOIN `degrees`B
     ON `A`.`id` = `B`.`department_id`
-WHERE `A`.`name` = 'Dipartimento di Neuroscienze'
+WHERE `A`.`name` = 'Dipartimento di Neuroscienze';
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
@@ -24,7 +24,7 @@ JOIN `course_teacher`
 	ON `courses`.`id` = `course_teacher`.`course_id`
 JOIN `teachers`
 	ON  `teachers`.`id` = `course_teacher`.`teacher_id`
-WHERE `teachers`.`id` = 44
+WHERE `teachers`.`id` = 44 ;
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea 
 -- a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
@@ -35,9 +35,18 @@ JOIN `degrees`
     ON  `degrees`.`id` = `students`.`degree_id`
 JOIN `departments`
     ON `departments`.`id` = `degrees`.`department_id` 
-ORDER BY `students`.`surname`, `students`.`name`
+ORDER BY `students`.`surname`, `students`.`name`;
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+SELECT `degrees`.`name` AS 'corso_di_laurea', `courses`.`name` AS 'Materia', `teachers`.`name` AS 'nome_insegnante'
+FROM `degrees`
+JOIN `courses`
+    ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `course_teacher`
+    ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+    ON `teachers`.`id`=  `course_teacher`.`teacher_id`;
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 
