0457db2875d98d4b50f413fd3a6161a1d4fcaafc
torone89 <torone89>
2022-09-07 14:56:15 +0200

    1 Ok

--- join.sql
+++ join.sql
@@ -1,5 +1,11 @@
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
 
+SELECT `students`.*
+    FROM `degrees`
+    JOIN `students`
+    ON `degrees`.`id` = `students`.`degree_id`
+  WHERE `degrees`.`name` = 'Corso di Laurea in Economia'
+
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
